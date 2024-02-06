ce2fb54598ebcc5e36cbdbd5129e5d64cbd0eeb5
gk3001 <gk3001>
2024-02-06 13:24:01 -0500

    changes

--- router/auth_users.js
+++ router/auth_users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const session = require('express-session')
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
@@ -7,22 +8,75 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let user = users.filter((user) => {
+
+    return user.username === username;
+})
+
+if(user.length > 0){
+
+    return true;
+}
+else{
+
+    return false;
+}
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+
+  let validate = users.filter((user) => {
+
+    return user.username === username && user.password === password
+
+  })
+
+  if(validate.length > 0){
+
+    return true;
+  }
+  else{
+
+    return false;
+  }
+  
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(!username || !password){
+
+    res.send("Error in login");
+  }
+
+  if(authenticatedUser(username,password)){
+
+    let accessToken = jwt.sign({
+        data : password
+    },'access', {expiresIn : 60 * 10})
+
+    req.session.authorization = {
+
+        accessToken, username
+    }
+
+    res.send("User Login")
+  }
+  else{
+
+    res.send("Invalid Credentials")
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.authenticated = regd_users;
